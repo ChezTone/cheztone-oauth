87a5a72144d36fae4475392d5582feab982133d4
Tony Proum <t.proum@gmail.com>
2015-08-23 11:09:15 +0200

    Add security, and allow user to authenticate with a default login form

--- src/main/java/org/cheztone/config/AppConfig.java
+++ src/main/java/org/cheztone/config/AppConfig.java
@@ -1,19 +1,12 @@
 package org.cheztone.config;
 
-import org.cheztone.oauth.OAuthServerConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.freemarker.FreeMarkerView;
-import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
 @EnableWebMvc
 @Configuration
-@ComponentScan(basePackages = {"org.cheztone.oauth"})
+@ComponentScan(basePackages = {"org.cheztone.oauth","org.cheztone.security"})
 public class AppConfig {
-
 
 }
---
+++ src/main/java/org/cheztone/config/SpringSecurityInitializer.java
@@ -0,0 +1,7 @@
+package org.cheztone.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+    //do nothing
+}

f25af2c3df150ddceec7651d4789b63ae806fe49
tony <t.proum@gmail.com>
2015-09-16 17:53:31 +0200

    Fix : - fix dependencies - fix config and - update redirect configuration

--- src/main/java/org/cheztone/SecurityConfig.java
+++ src/main/java/org/cheztone/SecurityConfig.java
@@ -1,7 +1,9 @@
 package org.cheztone;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,7 +16,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         // Example of use : http://localhost:8080/oauth/authorize?response_type=code&client_id=tone
-        auth.inMemoryAuthentication().withUser("tone").password("tone").roles("CLIENT");
+        auth.inMemoryAuthentication().withUser("tone").password("tone").roles("USER");
+    }
+
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Override
@@ -22,9 +31,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.authorizeRequests()
                 .antMatchers("/login").permitAll()
-                .antMatchers("/**").access("hasRole('ROLE_CLIENT')")
+                .antMatchers("/**").access("hasRole('ROLE_USER')")
                 .and()
-                .formLogin().loginPage("/login").failureUrl("/login?error")
+                .formLogin().loginPage("/login").failureUrl("/login?error").defaultSuccessUrl("/api", true)
                 .usernameParameter("username").passwordParameter("password")
                 .and()
                 .logout().logoutSuccessUrl("/login?logout")
--- src/main/webapp/WEB-INF/jsp/api.jsp
+++ src/main/webapp/WEB-INF/jsp/api.jsp
@@ -1,10 +1,6 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: 20004507
-  Date: 15/09/2015
-  Time: 16:47
-  To change this template use File | Settings | File Templates.
---%>
+<%@ taglib prefix="authz"
+           uri="http://www.springframework.org/security/tags"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
@@ -13,7 +9,9 @@
     <meta name="_csrf_header" content="${_csrf.headerName}"/>
 </head>
 <body>
-<button onclick="getToken('${_csrf.token}')">getToken</button>
+    <authz:authorize access="hasRole('ROLE_USER')">
+        <button onclick="getToken('${_csrf.token}')">getToken</button>
+    </authz:authorize>
 </body>
 <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
 <script src="${pageContext.request.contextPath}/resources/js/api.js"></script>
